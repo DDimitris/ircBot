0f9579435f90d890ebe8ac1a04c0c485ad3e88c1
Dedousis Dimitris <dimitris.dedousis@gmail.com>
2013-08-31 23:17:34 +0300

    Some first commands for fun

--- src/MyBot.java
+++ src/MyBot.java
@@ -8,9 +8,16 @@ public class MyBot extends PircBot {
     
     public void onMessage(String channel, String sender,
                        String login, String hostname, String message) {
-        if (message.equalsIgnoreCase("bot time")) {
-            String time = new java.util.Date().toString();
-            sendMessage(channel, sender + ": The time is now " + time);
-        }
+       if (message.matches("^Java_Bot\\sdate")){
+		String time = new java.util.Date().toString();
+                sendMessage(channel, sender + ": The date and time is: " + time);
+		}
+       else if (message.matches("Java_Bot\\shelp")){
+		sendMessage(channel, sender + ": The only thing i can do right now is:");
+		sendMessage(channel, "'Java_Bot date' and 'Java_Bot help' sorry!!");
+		}
+       else if (message.matches("^Java_Bot.*")) { 
+                 sendMessage(channel, sender + ": I'm under construction. Leave me alone! Type Java_Bot help for more info!!");
+         	}	
     }
 }
